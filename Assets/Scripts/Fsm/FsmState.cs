556e84386e06c47d35ec1a6b7015366233c6cf54
Dmitry <platelen@gmail.com>
2024-01-16 09:44:46 +0300

    Start
    Commit.

---
+++ Assets/Scripts/Fsm/FsmState.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Fsm
+{
+    public class FsmState :MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
